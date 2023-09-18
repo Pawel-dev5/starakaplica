34a31722174650eab62bf4ee9142e85f4cde8886
PawelNowecki-10a <pawel.nowecki@10a.io>
2023-09-18 11:59:02 +0200

    [FIX] MobileNav - fix width menu on iphone

--- components/Layout/Styles.js
+++ components/Layout/Styles.js
@@ -7,6 +7,7 @@ export const StyledMobileBodyWrapper = styled.div`
 	flex-direction: column;
 	align-items: center;
 	gap: 0.6rem;
+	width: 15rem;
 `;
 
 // NAVIGATION
@@ -150,6 +151,7 @@ export const StyledNavParentWrapper = styled.div`
 	align-items: center;
 	justify-content: center;
 	margin-bottom: 0.5rem;
+	width: 15rem;
 `;
 
 export const StyledDropdown = styled.div`
